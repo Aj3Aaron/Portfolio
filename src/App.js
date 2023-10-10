004db6afa9d2d20dd68d0b5c2caefbbe73039303
aaron hidalgo <Aj3Aaron>
2023-10-10 16:29:39 -0400

    commit con CNAME

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Header from "./components/Header";
-import AboutMe from "./components/AboutMe";
-import Nav from "./components/nav";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Certifications from "./components/Certifications";
-import Footer from "./components/Footer";
+import Header from "./sections/Header";
+import AboutMe from "./sections/AboutMe";
+import Nav from "./sections/nav";
+import Projects from "./sections/Projects";
+import Skills from "./sections/Skills";
+import Certifications from "./sections/Certifications";
+import Footer from "./sections/Footer";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.css';

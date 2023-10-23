f879bb591e9b6d31884da3e6eced8a3723f4d983
aaron hidalgo <Aj3Aaron>
2023-10-23 08:54:44 -0400

    actulizando readme.md

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,19 +1,84 @@
+import React, { useState, useEffect  } from 'react';
+import Modal from 'react-modal';
+import { CSSTransition } from 'react-transition-group';
 
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 import "../styles/Project.css";
+import '../styles/Modal.css';
+
+Modal.setAppElement('#root');
+
 
 function Project(props){
 
-    const {urlImg, projectTitle} = props;
+    const {urlImg, projectTitle, urlImg1, urlImg2, urlImg3} = props;
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [style, setStyle] = useState('span-component');
+
+    function display_menu(display){
+        // Acceder al elemento con la clase .menu
+        const menuElement = document.querySelector('.menu');
+        
+        // Hacer algo con el elemento, por ejemplo, ocultarlo
+        if (menuElement) {
+          menuElement.style.display = display;
+        }
+    }
+
+    const openModal = () => {
+        setModalIsOpen(true);
+        setStyle('project-open');
+        display_menu('none');
+    };
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+        setStyle('span-component');
+        display_menu('flex');
+    };
+
+
 
     return(
         <div>
             <div className="div-component">
-                <span className="span-component">
+                <span className={style} onClick={openModal}>
                     <img className="photo-component" src={urlImg} alt="Photo" />
                     <p> {projectTitle} </p>
                 </span>
             </div>
+
+            <CSSTransition
+                in={modalIsOpen}
+                timeout={300}
+                closeTimeoutMS={300}
+                classNames="modal"
+                unmountOnExit
+            >
+
+                <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    contentLabel="Ejemplo de Modal"
+                >
+                    <div className='div-content-modal'>
+                        <Carousel>
+                            <div>
+                                <img className="photo-modal" src={urlImg} alt="Photo" />
+                            </div>
+                            <div>
+                                <img className="photo-modal" src={urlImg1} alt="Photo" />
+                            </div>
+                            <div>
+                                <img className="photo-modal" src={urlImg} alt="Photo" />
+                            </div>
+                        </Carousel>
+                    </div>
+                    <button onClick={closeModal}>Cerrar Modal</button>
+                </Modal>
+            </CSSTransition>
         </div>
     )
 }
--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -2,14 +2,16 @@ import Project from "../components/Project";
 import "../styles/Project.css";
 
 function Projects(){
+
+
     return(
         <section className='contenedor-header contenedor-Projects'>
             <h1 className='h1-Projects'>Proyectos realizados</h1>
             <div className="div-projects">
-                <Project  urlImg={ require ('../img/ejemplo_portafolio.png')} projectTitle="Portafolio en React" />
-                <Project urlImg={ require ('../img/bot_cok.png')} projectTitle="Bot Clash of Kings" />
-                <Project urlImg={ require ('../img/home_tuGruero.png')} projectTitle="Tu Gruero (se ha censurado informacion sensible)" />
-                <Project urlImg={ require ('../img/CambioDolar.png')} projectTitle="App mobile multiplataforma" />
+                <Project  urlImg={ require ('../img/ejemplo_portafolio.png')} urlImg1={ require ('../img/cambioDolar.jpg') } projectTitle="Portafolio en React" />
+                <Project urlImg={ require ('../img/bot_cok.png')} urlImg1={ require('../img/cambioDolar.jpg') }  projectTitle="Bot Clash of Kings" />
+                <Project urlImg={ require ('../img/home_tuGruero.png')} urlImg1={ require('../img/cambioDolar.jpg') }  projectTitle="Tu Gruero (se ha censurado informacion sensible)" />
+                <Project urlImg={ require ('../img/CambioDolar.png')} urlImg1={ require('../img/cambioDolar.jpg') }  projectTitle="App mobile multiplataforma" />
             </div>
         </section>
     )

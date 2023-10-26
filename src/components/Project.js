9e3f6aee61e124f79f03e8236eee2c5a58cf0afb
aaron hidalgo <Aj3Aaron>
2023-10-26 18:54:11 -0400

    modales listos

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,6 +5,8 @@ import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import Skill from './Skill';
 
 import "../styles/Project.css";
 import '../styles/Modal.css';
@@ -15,7 +17,7 @@ Modal.setAppElement('#root');
 
 function Project(props){
 
-    const {urlImg, projectTitle, urlImg1, urlImg2, urlImg3} = props;
+    const {urlImg, projectTitle, projectDescription, tuGruero, skills, linkProject, urlImg1, urlImg2, urlImg3} = props;
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const [style, setStyle] = useState('span-component');
 
@@ -71,13 +73,34 @@ function Project(props){
                     <div className='div-content-modal'>
                         
                         <Carousel>
-                            <div>
+                            <div className='div-photo-modal'>
+                                <div className='div-background-icons'>
+                                    <div className='div-icons'>
+                                        <p className='icon-red'></p>
+                                        <p className='icon-yellow'></p>
+                                        <p className='icon-green'></p>
+                                    </div>
+                                </div>
                                 <img className="photo-modal" src={urlImg} alt="Photo" />
                             </div>
-                            <div>
+                            <div className='div-photo-modal'>
+                                <div className='div-background-icons'>
+                                    <div className='div-icons'>
+                                        <p className='icon-red'></p>
+                                        <p className='icon-yellow'></p>
+                                        <p className='icon-green'></p>
+                                    </div>
+                                </div>
                                 <img className="photo-modal" src={urlImg1} alt="Photo" />
                             </div>
-                            <div>
+                            <div className='div-photo-modal'>
+                                <div className='div-background-icons'>
+                                    <div className='div-icons'>
+                                        <p className='icon-red'></p>
+                                        <p className='icon-yellow'></p>
+                                        <p className='icon-green'></p>
+                                    </div>
+                                </div>
                                 <img className="photo-modal" src={urlImg} alt="Photo" />
                             </div>
                         </Carousel>
@@ -85,6 +108,28 @@ function Project(props){
                             <span onClick={closeModal} className='modal-close'><FontAwesomeIcon  icon={faTimes} /></span>
                         </div>
                     </div>
+                    <h1 className='h1-modal' >{projectTitle} <a className='icon-link' target="_blank" rel="noopener noreferrer" href={linkProject}><FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a></h1>
+                    {tuGruero ? (
+                        <p>
+                            {projectDescription.tuGruero_description_1}
+                            <a target="_blank" rel="noopener noreferrer" href={projectDescription.tuGrueroLink}>
+                            TuGruero.io
+                            </a>
+                            {projectDescription.tuGruero_description_2}
+                        </p>
+                        ) : (
+                        <p>{projectDescription}</p>
+                    )}
+
+                    <div className='skills-modal' >
+                        {skills.map((element, index) => (
+                            <Skill key={index} urlImg={element} />
+                        ))}
+                    </div>
+
+                    
+
+                    
                 </Modal>
             </CSSTransition>
         </div>
--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -1,18 +1,28 @@
 import Project from "../components/Project";
-import React, { useState } from 'react';
 
 import "../styles/Project.css";
 
 function Projects(){
 
+    const tuGruero_description = {
+        tuGruero_description_1: "1 año de experiencia desarrollando para ",
+        tuGruero_description_2:" manejando base de datos con SQL, utilizando lenguajes como PHP, JavaScript, HTML y CSS. Ademas de mantenimiento landing page con WordPress.",
+        tuGrueroLink: "https://tugruero.io/"
+    };
+
+   const skillPage = [require('../img/HTML.png'), require('../img/CSS.png'), require('../img/bootstrap.png'), require('../img/javaScript.png'), require('../img/react.png'), require('../img/git.png')]
+   const skillBot = [require('../img/python.png'), require('../img/git.png'), require ('../img/android.png')];
+   const skillTuGruero = [require('../img/HTML.png'), require('../img/CSS.png'), require('../img/bootstrap.png'), require('../img/javaScript.png'), require('../img/php.png'), require('../img/sql.png'), require('../img/wordpress.png'), require('../img/aws.png')];
+   const skillCambioDolar = [require('../img/CSS.png'), require('../img/react.png'), require('../img/javaScript.png'), require('../img/android.png')]
+
     return(
         <section className='contenedor-header contenedor-Projects'>
             <h1 className='h1-Projects'>Proyectos realizados</h1>
             <div className="div-projects">
-                <Project urlImg={ require ('../img/ejemplo_portafolio.png')} urlImg1={ require ('../img/cambioDolar.jpg') } projectTitle="Portafolio en React" />
-                <Project urlImg={ require ('../img/bot_cok.png')} urlImg1={ require('../img/cambioDolar.jpg') }  projectTitle="Bot Clash of Kings" />
-                <Project urlImg={ require ('../img/home_tuGruero.png')} urlImg1={ require('../img/cambioDolar.jpg') }  projectTitle="Tu Gruero (se ha censurado informacion sensible)" />
-                <Project urlImg={ require ('../img/CambioDolar.png')} urlImg1={ require('../img/cambioDolar.jpg') }  projectTitle="App mobile multiplataforma" />
+                <Project urlImg={ require ('../img/ejemplo_portafolio.png')} skills={skillPage} urlImg1={ require ('../img/cambioDolar.jpg') } linkProject='https://github.com/Aj3Aaron/Portfolio' projectTitle="Portafolio en React." projectDescription='Portafolio en React que puede ser utilizado, por ejemplo, por otros usuarios de GitHub. El portafolio incluye secciones separadas (Acerca de, Proyectos, Habilidades, Experiencia).' />
+                <Project urlImg={ require ('../img/bot_cok.png')} skills={skillBot} urlImg1={ require('../img/cambioDolar.jpg') } linkProject='https://github.com/hidalgoabraham/aj-cok-farming-bot' projectTitle="Bot Clash of Kings." projectDescription='Bot de reconocimiento de patrones basado en imagenes, fue desarrollado en python con librerias como OpenCv, OCR y Adb para conectarse a un dispositivo android.' />
+                <Project urlImg={ require ('../img/home_tuGruero.png')} skills={skillTuGruero} urlImg1={ require('../img/cambioDolar.jpg') } linkProject={tuGruero_description.tuGrueroLink} tuGruero={true}  projectTitle="Tu Gruero." projectDescription={tuGruero_description} />
+                <Project urlImg={ require ('../img/CambioDolar.png')} skills={skillCambioDolar} urlImg1={ require('../img/cambioDolar.jpg') } linkProject='https://github.com/Aj3Aaron/Cambio-dolar' projectTitle="App mobile multiplataforma." projectDescription='App multiplataforma desarrollada con React Native para saber en tiempo real el tipo de cambio del Dolar en Venezuela con tasas de distintas entidades.' />
             </div>
         </section>
     )
